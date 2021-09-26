8a3890fdfbdb79d41ec4ae27832eeca06d887c14
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-09-26 06:29:42 +0530

    cli changes

--- bin/cli.js
+++ bin/cli.js
@@ -16,6 +16,8 @@ return true
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/ShivanshVerma-coder/cra-for-extension ${repoName}`
 const installDepsCommand = `cd ${repoName} && npm install`;
+const removeRemote = `git remote remove origin`
+const gitCommands = 'git add . && git commit -m \'first-commit\' ';
 
 console.log('Setting up react extension app for you...')
 
@@ -25,5 +27,9 @@ if(!checkedOut) process.exit(-1);
 console.log('Installing dependencies...')
 const installedDeps = runCommand(installDepsCommand)
 if(!installedDeps) process.exit(-1)
+
+console.log('finalizing things')
+const removedRemote = runCommand(removeRemote)
+if(!removedRemote) process.exit(-1)
 
 console.log('Congratulations you are all set to go!!!')
